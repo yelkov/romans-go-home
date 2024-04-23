1f68b290b40661011a5f447c295295f588e102bc
yelkov <yelkovq@gmail.com>
2024-04-23 13:47:59 +0200

    test(PatterRomanNumberTest.java): add test fourSymbols

--- app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
+++ app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
@@ -6,15 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PatternRomanNumberTest {
     @Test
-    public void test_cleanRoman(){
+    public void test_cleanRoman_simple(){
         String cleanRoman = PatternRomanNumber.cleanRomanNumber("UMMMU");
         assertEquals("MMM",cleanRoman);
         String cleanRoman1 = PatternRomanNumber.cleanRomanNumber("UMMU");
         assertEquals("MM",cleanRoman1);
-        String cleanRoman2 = PatternRomanNumber.cleanRomanNumber("UMMMMU");
-        assertEquals("MMM",cleanRoman2);
         String cleanRoman3 = PatternRomanNumber.cleanRomanNumber("UU");
         assertEquals("",cleanRoman3);
+    }
+    @Test
+    public void test_cleanRoman_fourSymbols() {
+        String cleanRoman = PatternRomanNumber.cleanRomanNumber("UMMMMU");
+        assertEquals("MMM", cleanRoman);
+        String cleanRoman1 = PatternRomanNumber.cleanRomanNumber("UMMMMUCCCC");
+        assertEquals("MMMCCC",cleanRoman1);
     }
 
 }

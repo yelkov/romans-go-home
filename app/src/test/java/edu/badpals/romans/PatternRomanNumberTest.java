64750e5fd19f5189210455ead15f8ad899de72df
Yelko Veiga <yelkovq@gmail.com>
2024-04-23 17:20:20 +0200

    test(PatternRomanNumberTest): delete MMMM test

--- app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
+++ app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
@@ -11,8 +11,6 @@ class PatternRomanNumberTest {
         assertEquals("MMM",cleanRoman);
         String cleanRoman1 = PatternRomanNumber.cleanRomanNumber("UMMU");
         assertEquals("MM",cleanRoman1);
-        String cleanRoman2 = PatternRomanNumber.cleanRomanNumber("UMMMMU");
-        assertEquals("MMM",cleanRoman2);
         String cleanRoman3 = PatternRomanNumber.cleanRomanNumber("UU");
         assertEquals("",cleanRoman3);
     }

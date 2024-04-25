3193ddb14ad5c8a513ed542402c87a3cbc52b129
AmarinoLama <amarinolama@gmail.com>
2024-04-25 16:09:34 +0200

    test (PatternRomanNumberTest) : change name of the method

--- app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
+++ app/src/test/java/edu/badpals/romans/PatternRomanNumberTest.java
@@ -7,19 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class PatternRomanNumberTest {
     @Test
     public void test_cleanRoman_simple(){
-        String cleanRoman = PatternRomanNumber.cleanRomanNumber("UMMMU");
+        String cleanRoman = PatternRomanNumber.sumatoryPattern("UMMMU");
         assertEquals("MMM",cleanRoman);
-        String cleanRoman1 = PatternRomanNumber.cleanRomanNumber("UMMU");
+        String cleanRoman1 = PatternRomanNumber.sumatoryPattern("UMMU");
         assertEquals("MM",cleanRoman1);
-        String cleanRoman3 = PatternRomanNumber.cleanRomanNumber("UU");
+        String cleanRoman3 = PatternRomanNumber.sumatoryPattern("UU");
         assertEquals("",cleanRoman3);
     }
-    @Test
-    public void test_cleanRoman_fourSymbols() {
-        String cleanRoman = PatternRomanNumber.cleanRomanNumber("UMMMMU");
-        assertEquals("MMM", cleanRoman);
-        String cleanRoman1 = PatternRomanNumber.cleanRomanNumber("UMMMMUCCCC");
-        assertEquals("MMMCCC",cleanRoman1);
-    }
-
 }

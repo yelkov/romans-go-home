352b31c29e3332a19977b9830be492117b51fec6
AmarinoLama <amarinolama@gmail.com>
2024-04-25 16:59:15 +0200

    feat (PatterRomanNumber) : clear the list in the method getRomanNumbers

--- app/src/main/java/edu/badpals/romans/PatternRomanNumber.java
+++ app/src/main/java/edu/badpals/romans/PatternRomanNumber.java
@@ -1,20 +1,39 @@
 package edu.badpals.romans;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static edu.badpals.romans.PatternRomanNumber.substractPattern;
+import static edu.badpals.romans.PatternRomanNumber.sumatoryPattern;
+
 public class PatternRomanNumber {
 
-    private static String sumatory = "([IXCM]{0,3}|[VLD])";
+    private static String sumatory = "([IXCM]{1,3}|[VLD])";
+    private static String substract = "(I[VX]|X[LC]|C[DM])";
+    private static ArrayList<String> romanNumbers = new ArrayList<>();
 
-    public static String sumatoryPattern(String roman){
+    public static void substractPattern(String roman){
+        Pattern p = Pattern.compile(substract);
+        Matcher m = p.matcher(roman);
+
+        while(m.find()){
+            romanNumbers.add(m.group());
+        }
+    }
+    public static void sumatoryPattern(String roman){
         Pattern p = Pattern.compile(sumatory);
         Matcher m = p.matcher(roman);
 
-        String cleanRoman ="";
         while(m.find()){
-            cleanRoman += m.group();
+            romanNumbers.add(m.group());
         }
-        return cleanRoman;
+    }
+
+    public static ArrayList<String> getRomanNumbers(String roman){
+        romanNumbers.clear();
+        substractPattern(roman);
+        sumatoryPattern(roman);
+        return romanNumbers;
     }
 }

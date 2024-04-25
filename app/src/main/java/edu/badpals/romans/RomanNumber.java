339101e099f943fd73e4f505d91ab166f4baed6b
AmarinoLama <amarinolama@gmail.com>
2024-04-25 16:58:35 +0200

    feat (RomanNumber) : make toDecimal accept subtract values

--- app/src/main/java/edu/badpals/romans/RomanNumber.java
+++ app/src/main/java/edu/badpals/romans/RomanNumber.java
@@ -1,6 +1,6 @@
 package edu.badpals.romans;
 
-import java.util.EnumSet;
+import java.util.ArrayList;
 
 public class RomanNumber {
     String roman;
@@ -8,10 +8,14 @@ public class RomanNumber {
         this.roman = roman;
     }
     public Short toDecimal(){
-        String cleanNumber = PatternRomanNumber.cleanRomanNumber(roman);
+        ArrayList<String> cleanNumber = PatternRomanNumber.getRomanNumbers(roman);
         short value = 0;
-        for (Character c : cleanNumber.toCharArray()){
-            value += RomanSymbols.valueOf(c.toString()).getValue();
+        for (String pattern : cleanNumber){
+            if (pattern.length() == 2 && pattern.charAt(0) != pattern.charAt(1)){
+                value += RomanSymbols.valueOf(pattern).getValue();
+            } else {
+                value += RomanSymbols.valueOf(Character.toString(pattern.charAt(0))).getValue() * pattern.length();
+            }
         }
         return value;
     }

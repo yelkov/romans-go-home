17b1b48e288ee4658b008a11db8e150043db99df
AmarinoLama <amarinolama@gmail.com>
2024-04-25 16:13:18 +0200

    feat (RomanSymbols) : add subtracted symbols

--- app/src/main/java/edu/badpals/romans/RomanSymbols.java
+++ app/src/main/java/edu/badpals/romans/RomanSymbols.java
@@ -7,7 +7,13 @@ public enum RomanSymbols {
     L(50),
     C(100),
     D(500),
-    M(1000);
+    M(1000),
+    IV(4),
+    IX(9),
+    XL(40),
+    XC(90),
+    CD(400),
+    CM(900);
 
     int value;
     private RomanSymbols(int value){
